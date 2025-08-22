e95d57358405f36fdeb0ff31abbc51cb7a8d6016
d0ty <hp.raszpi@gmail.com>
2025-08-22 14:02:27 +0200

    Tested the websocket infra
    Added test for reading from the Onionsat GRU and not logging in
    
    Changed the Websocket connection to Sugar Websocket to have more precise
    control over the connection lifecycle. Also made async the close method,
    to make sure the connection really closes before finishing the test.

---
+++ packages/device-comm/src/ws_client.ts
@@ -0,0 +1,53 @@
+import { SugarWs } from "https://deno.land/x/sugar_ws/mod.ts";
+
+export default class WebsocketClient {
+  conn: SugarWs;
+  closed: boolean;
+
+  constructor(url: string) {
+    this.conn = new SugarWs(url);
+    this.closed = false;
+  }
+
+  async read(): Promise<string> {
+    const response = await new Promise<string>((resolve, reject) => {
+      this.conn.onmessage = (event) => {
+        resolve(event.data);
+      };
+      this.conn.onerror = (err) => {
+        reject(new Error("WebSocket error: " + err));
+      };
+    });
+    return response;
+  }
+
+  async execCmd(cmd: string, params: string[]): Promise<string> {
+    if (this.closed) {
+      throw new Error("Connection closed");
+    } else {
+      let dataToSend: string = cmd + "(";
+      for (let i = 0; i < params.length; i++) {
+        dataToSend += params[i];
+        if (i < params.length - 1) {
+          dataToSend += ", ";
+        }
+      }
+      dataToSend += ")";
+      this.conn.send(dataToSend);
+      return await this.read();
+    }
+  }
+  async login(exp_id: string, api_key: string): Promise<boolean> {
+    try {
+      const responseText = await this.execCmd("login", [exp_id, api_key]);
+      return responseText === "true";
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async close() {
+    await this.conn.wait_for("close").and_close();
+    this.closed = true;
+  }
+}
