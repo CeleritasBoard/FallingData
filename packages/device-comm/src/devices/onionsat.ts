b6d1cf2549fc1ddc5b5a9e22b20dfd7e848b5654
d0ty <hp.raszpi@gmail.com>
2025-08-24 20:55:35 +0200

    The little debugging is finished.
    Changes:
     - `device.ts`: introduced a new pror called `device_name: string` which
       is a string identifier for each device subclass. Used for the API key
       getting.
     - `.env.example`: an example setup of what your .env.local file should
       look like. Do not put any credenetials here.
     - ˙onionsat.ts`: adjustments for `device.ts` (see above) and added an
       extra websocket read before the common init routine in order to
       handle the ONIONSAT welcome message
     - `ws_client.ts`: adjusted the login response validation to the real
       case and if something goes wrong throw the error instead of just
       returning false.
     - `devices_test.ts`: closed the connection at the end of the test

--- packages/device-comm/src/devices/onionsat.ts
+++ packages/device-comm/src/devices/onionsat.ts
@@ -1,40 +1,46 @@
 import DeviceBase from "./device.ts";
 import WebsocketClient from "../ws_client.ts";
 
-export default class OnionSatDevice extends DeviceBase{
-    server_link: string;
-    exp_id: string;
-    conn: WebsocketClient;
+export default class OnionSatDevice extends DeviceBase {
+  protected server_link: string;
+  protected exp_id: string;
+  protected device_name: string;
+  protected conn: WebsocketClient;
 
+  constructor() {
+    super();
+    this.server_link = "ws://gru.onionsat.com:8080/";
+    this.exp_id = "celeritas";
+    this.device_name = "ONIONSAT";
+    this.conn = new WebsocketClient(this.server_link);
+  }
 
-    constructor(){
-        super();
-        this.server_link = "ws://gru.onionsat.com:8080/";
-        this.exp_id = "celeritas";
-        this.conn = new WebsocketClient(this.server_link);
+  override async init(): Promise<boolean> {
+    await this.conn.read();
+    return await super.init();
+  }
 
+  async loadData(start: number | null, end: number | null): Promise<boolean> {
+    let result: boolean = false;
+    if (this.inited) {
+      if (start == null) {
+        start = 1;
+      }
+      if (end == null) {
+        end = Math.floor(Date.now() / 1000);
+      }
+      const message: string = await this.conn.execCmd("getEXPData", [
+        this.exp_id,
+        start.toString(),
+        end.toString(),
+      ]);
+      console.log("sending data. Message: " + message);
+      result = true;
     }
+    return result;
+  }
 
-    async loadData(start: number | null, end: number | null): Promise<boolean>{
-        let result: boolean = false;
-        console.log("loading data", this.inited);
-        if(this.inited){
-            if(start == null){
-                start = 1;
-            }
-            if(end == null){
-                end = Math.floor(Date.now() / 1000);
-            }
-            const message: string = await this.conn.execCmd("getEXPData", [start.toString(), end.toString()]);
-            console.log("sending data. Message: " + message);
-            result = true;
-        }
-        return result;
-
-    }
-
-    async sendCMD(cmd: string):Promise<boolean>{
-        return false;
-    }
-
+  async sendCMD(cmd: string): Promise<boolean> {
+    return false;
+  }
 }
--- packages/device-comm/test/devices_test.ts
+++ packages/device-comm/test/devices_test.ts
@@ -1,8 +1,9 @@
 import { assertEquals } from "@std/assert";
 import OnionsatDevice from "../src/devices/onionsat.ts";
 
-Deno.test("Onionsat Packet Download", async function onionsat_test()  {
-    let os = new OnionsatDevice();
-    await os.init();
-    assertEquals((await os.loadData(4, 12)), true);
+Deno.test("Onionsat Packet Download", async function onionsat_test() {
+  let os = new OnionsatDevice();
+  await os.init();
+  assertEquals(await os.loadData(4, 12), true);
+  await os.close();
 });
