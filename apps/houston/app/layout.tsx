195b12915126a4ed789e4fe51b757e3e2886c97e
d0ty <hp.raszpi@gmail.com>
2025-09-05 18:24:59 +0200

    it works... why? it doesn't works... why?
    DO NOT ASK ABOUT IT! IF IT WORKS, DO NOT TOUCH IT!

--- apps/houston/app/layout.tsx
+++ apps/houston/app/layout.tsx
@@ -3,12 +3,12 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
-
 export const metadata: Metadata = {
-  metadataBase: new URL(defaultUrl),
+  metadataBase: new URL(
+    process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000",
+  ),
   title: "Next.js and Supabase Starter Kit",
   description: "The fastest way to build apps with Next.js and Supabase",
 };
@@ -19,11 +19,11 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): Promise<React.ReactNode> {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
--- apps/houston/app/page.tsx
+++ apps/houston/app/page.tsx
@@ -1,14 +1,20 @@
-
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@workspace/ui/components/alert";
 
 export default async function Home() {
+  return (
+    <div>
+      <h1>Welcome to Houston</h1>
 
-    return (
-        <div>
-            <h1>Welcome to Houston</h1>
-
-
-        </div>
-    );
+      <Alert variant="destructive">
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
 }
-
-
