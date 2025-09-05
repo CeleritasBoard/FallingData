ce8c87a1c2f8258f9c5642b4278b5487ce581630
d0ty <hp.raszpi@gmail.com>
2025-09-05 18:38:37 +0200

    shadcn and tailwind works in both houston and outreach also did a bit of a cleanup in outreach
    resolves #12

--- apps/outreach/app/page.tsx
+++ apps/outreach/app/page.tsx
@@ -1,7 +1,10 @@
+import { Badge } from "@workspace/ui/components/badge";
+
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       Welcome tutorial!
+      <Badge variant="secondary">A secondary badge</Badge>
     </main>
   );
 }
