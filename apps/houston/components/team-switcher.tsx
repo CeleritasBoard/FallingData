8ebc2498623b146dbb1ee17bb904efced0aae012
d0ty <hp.raszpi@gmail.com>
2025-10-02 20:43:41 +0200

    finished "team switcher" look

--- apps/houston/components/team-switcher.tsx
+++ apps/houston/components/team-switcher.tsx
@@ -7,11 +7,13 @@ import Logo from "../public/logo.png";
 export function TeamSwitcher() {
   return (
     <div className="gap-2 p-2">
-      <div className="flex size-6 items-center justify-center rounded-md border">
-        <Image src={Logo} alt="Logo" className="size-3.5 shrink-0" />
+      <div className="flex  items-center rounded-md">
+        <div className="rounded-md bg-[#6A6A6A] mr-5">
+          <Image src={Logo} alt="Logo" className="size-10 shrink-0" />
+        </div>
         <div className="block">
-          <div>Celeritas</div>
-          <div>Houston</div>
+          <div className="font-bold text-sm">Celeritas</div>
+          <div className="text-xs">Houston</div>
         </div>
       </div>
     </div>
