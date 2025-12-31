49608dfe0f48e69c4b02169cc6d1955c1fc60bb2
d0ty <hp.raszpi@gmail.com>
2025-12-31 16:19:31 +0100

    Finalized the import UI

--- apps/houston/app/packets/import/page.tsx
+++ apps/houston/app/packets/import/page.tsx
@@ -30,23 +30,29 @@ export default function ImportPage() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0 w-full">
       <div className="flex flex-col gap-2">
-        <h1 className="text-5xl font-bold tracking-tight text-foreground">
+        <h1 className="text-5xl font-bold tracking-tight text-foreground mb-16">
           Packetek importálása
         </h1>
-        <form onSubmit={submitCallback}>
-          <input
-            name="file"
-            type="file"
-            onChange={async (e) =>
-              setFileContent((await e.target.files?.[0]?.text()) ?? null)
-            }
-          />
-          <input
-            name="date"
-            type="datetime-local"
-            onChange={(e) => setDate(e.target.value)}
-          />
-          <Button>Importálás</Button>
+        <form onSubmit={submitCallback} className="flex flex-col gap-4">
+          <div className="block">
+            <label className="block">Fájl:</label>
+            <input
+              name="file"
+              type="file"
+              onChange={async (e) =>
+                setFileContent((await e.target.files?.[0]?.text()) ?? null)
+              }
+            />
+          </div>
+          <div className="block">
+            <label className="block">Dátum:</label>
+            <input
+              name="date"
+              type="datetime-local"
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+          <Button className="self-start mt-4">Importálás</Button>
         </form>
       </div>
     </div>
