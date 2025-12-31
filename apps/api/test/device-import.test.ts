5b6155244f5052786659744c18c658013732565c
d0ty <hp.raszpi@gmail.com>
2025-12-31 12:22:13 +0100

    Unit test for sloth data import
    Also moved the test.cel (created in task 33) to under a central
    `test_data` directory that works as a central place for unified test
    data.

--- apps/api/test/device-import.test.ts
+++ apps/api/test/device-import.test.ts
@@ -2,6 +2,8 @@ import { expect, test, beforeAll } from "vitest";
 import fetch from "node-fetch";
 import { loadEnvConfig } from "@next/env";
 import { initSupaAuth, getSupaAuthCredentials } from "./utils/auth";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
 beforeAll(async () => {
   const projectDir = process.cwd();
@@ -9,7 +11,7 @@ beforeAll(async () => {
   await initSupaAuth();
 });
 
-test("Device Import", async () => {
+test("Onionsat Device Import", async () => {
   const token = await getSupaAuthCredentials();
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_HOST}/device-import`,
@@ -17,6 +19,32 @@ test("Device Import", async () => {
       method: "POST",
       body: JSON.stringify({
         startDate: new Date("2025-06-22").getTime() / 1000,
+        device: "ONIONSAT_TEST",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  expect(response.status).toBe(201);
+});
+
+test("Sloth Device Import", async () => {
+  const content = fs.readFileSync(
+    path.resolve(__dirname, "../../../test_data/test.cel"),
+    "utf8",
+  );
+
+  const token = await getSupaAuthCredentials();
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_HOST}/device-import`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        startDate: new Date("2025-12-31").getTime() / 1000,
+        device: "SLOTH",
+        content,
       }),
       headers: {
         "Content-Type": "application/json",
