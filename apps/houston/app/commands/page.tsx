c3299c4714ebe54f68bd5f0eac8e5260d5ce2e2a
d0ty <hp.raszpi@gmail.com>
2026-01-12 19:07:50 +0100

    implemented basic commands table
    Missing (will be added in later commits):
     - Type and State filter values
     - Created by field

---
+++ apps/houston/app/commands/page.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import DatabaseTable from "../../components/db-table";
+import { Tables } from "@repo/supabase/database.types.ts";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, ExternalLink } from "lucide-react";
+import { Button } from "../../../../packages/ui/src/components/button.tsx";
+
+const columns: ColumnDef<Tables<"commands">>[] = [
+  {
+    accessorKey: "id",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          ID
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    meta: {
+      filterVariant: "number",
+    },
+  },
+  {
+    accessorKey: "cmd_device",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Device
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    meta: {
+      filterVariant: "selectDevice",
+    },
+  },
+  {
+    accessorKey: "execution_time",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Date
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <span>{row.original.execution_time}</span>;
+    },
+    meta: {
+      filterVariant: "dateRange",
+    },
+  },
+  {
+    accessorKey: "type",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Type
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    meta: {
+      filterVariant: "selectType",
+    },
+  },
+  {
+    accessorKey: "command",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Command
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    meta: {
+      filterVariant: "text",
+    },
+  },
+  {
+    accessorKey: "state",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Status
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    meta: {
+      filterVariant: "selectType",
+    },
+  },
+  // TODO: user field
+  {
+    id: "link",
+    header: ({}) => ``,
+    cell: ({ row }) => {
+      return (
+        <Link href={`/packets/${row.original.id}`}>
+          <ExternalLink />
+        </Link>
+      );
+    },
+  },
+];
+
+export default function CommandTable() {
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 pt-0 w-full">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-5xl font-bold tracking-tight text-foreground">
+          Parancsok
+        </h1>
+      </div>
+      <DatabaseTable columns={columns} table="commands" />
+    </div>
+  );
+}
--- apps/houston/components/nav-main.tsx
+++ apps/houston/components/nav-main.tsx
@@ -55,9 +55,13 @@ const items: {
   },
   {
     title: "Parancsok",
-    url: "#",
+    url: "/commands",
     icon: Terminal,
     items: [
+      {
+        title: "Parancsok megtekintése",
+        url: "/commands",
+      },
       {
         title: "Új parancs felküldése",
         url: "#",
