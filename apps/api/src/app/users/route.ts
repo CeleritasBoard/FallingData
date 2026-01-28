30687ac81791cdcd1781f5e2d2e68640bd18cafc
d0ty <hp.raszpi@gmail.com>
2026-01-28 09:33:41 +0100

    user creation backend

--- apps/api/src/app/users/route.ts
+++ apps/api/src/app/users/route.ts
@@ -29,7 +29,10 @@ export async function GET(request: Request) {
         if (meta.invited && !(meta.full_name && meta.email)) user_state = "NEW";
         else if (meta.invited && meta.full_name && meta.email)
           user_state = "CONFIRMED";
-        else if (user.email == process.env.SUPABASE_TEST_EMAIL)
+        else if (
+          user.email == process.env.SUPABASE_TEST_EMAIL ||
+          user.email?.endsWith("@celeritas-board.hu")
+        )
           user_state = "TEST";
 
         return {
@@ -43,4 +46,59 @@ export async function GET(request: Request) {
     ),
     { status: 200 },
   );
+}
+
+export async function PUT(req: Request) {
+  try {
+    const user = await getUser(
+      await createClient(),
+      (await headers()) as Headers,
+    );
+
+    if (!user) return new Response("Unauthorized", { status: 401 });
+
+    const headerList = await headers();
+    if (
+      !(
+        headerList.has("Content-Type") &&
+        headerList.get("Content-Type") === "application/json"
+      )
+    )
+      return new Response("Unsupported media", { status: 415 });
+
+    let json: { email?: string } | undefined | null = await req.json();
+
+    if (!json || !json.email)
+      return new Response("Bad Request", { status: 400 });
+
+    console.log("Creating user with email:", json.email);
+    const admin = createAdminClient().auth.admin;
+    const { data: _, error } = await admin.createUser({
+      email: json.email!,
+      password: (() => {
+        const length = 12;
+        const chars =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}<>?";
+        let pwd = "";
+        const randomValues = crypto.getRandomValues(new Uint8Array(length));
+        for (let i = 0; i < length; i++) {
+          pwd += chars[randomValues[i] % chars.length];
+        }
+        return pwd;
+      })(),
+      email_confirm: true,
+      user_metadata: {
+        invited: true,
+      },
+    });
+
+    if (error) {
+      console.error(error);
+      return new Response("Bad Gateway", { status: 502 });
+    }
+    return new Response("Created", { status: 201 });
+  } catch (e) {
+    console.error(e);
+    return new Response("Bad Gateway", { status: 502 });
+  }
 }
--- apps/api/test/users.test.ts
+++ apps/api/test/users.test.ts
@@ -27,3 +27,30 @@ test("User Listing", async () => {
     json.find((item) => item.email == process.env.SUPABASE_TEST_EMAIL)?.status,
   ).toBe("TEST");
 });
+
+const generateRandomChars = (length: number) => {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let pwd = "";
+  const randomValues = crypto.getRandomValues(new Uint8Array(length));
+  for (let i = 0; i < length; i++) {
+    pwd += chars[randomValues[i] % chars.length];
+  }
+  return pwd;
+};
+
+test("User Creation", async () => {
+  const token = await getSupaAuthCredentials();
+
+  const resp = await fetch(`${process.env.NEXT_PUBLIC_HOST}/users`, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: `${generateRandomChars(10)}@celeritas-board.hu`,
+    }),
+  });
+  expect(resp.status).toBe(201);
+});
