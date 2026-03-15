3996fc35705f021107971715a64e5829bd9ab3d7
d0ty <hp.raszpi@gmail.com>
2026-03-15 11:22:30 +0100

    Graph table

---
+++ supabase/migrations/20260311062752_graphs.sql
@@ -0,0 +1,10 @@
+create type "graph_type" as enum('spectrum', 'custom');
+
+create table "graphs" (
+  "id" serial primary key,
+  "mission" integer not null references "missions",
+  "type" graph_type not null,
+  "description" text,
+  "featured" boolean default false,
+  "data" jsonb not null
+);
