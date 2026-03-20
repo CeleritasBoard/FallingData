d033f00f2bb98837db3f517b94ef1bd0db23ee07
d0ty <hp.raszpi@gmail.com>
2026-03-20 15:36:28 +0100

    outreach project kickoff #97

--- apps/outreach/app/page.tsx
+++ apps/outreach/app/page.tsx
@@ -1,10 +1,20 @@
+import { createClient } from "@/lib/supabase/server";
 import { Badge } from "@workspace/ui/components/badge";
 
-export default function Home() {
+export default async function Home() {
+  const supa = await createClient();
+  const { count: data, error } = await supa
+    .from("missions")
+    .select("*", { count: "exact" });
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
   return (
     <main className="min-h-screen flex flex-col items-center">
       Welcome tutorial!
-      <Badge variant="secondary">A secondary badge</Badge>
+      <Badge variant="secondary">Number of missions: {data}</Badge>
     </main>
   );
 }
