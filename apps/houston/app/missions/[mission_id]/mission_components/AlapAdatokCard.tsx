161e3e275595b934621d871ea1043bf5d5762d01
d0ty <hp.raszpi@gmail.com>
2026-03-30 20:36:28 +0200

    houston: schedule dialog should only be able to opened from created state

--- apps/houston/app/missions/[mission_id]/mission_components/AlapAdatokCard.tsx
+++ apps/houston/app/missions/[mission_id]/mission_components/AlapAdatokCard.tsx
@@ -140,9 +140,7 @@ export function AlapadatokCard({
           </p>
 
           <div className="flex items-center gap-3 pt-2">
-            {(data.status === "CREATED" ||
-              data.status === "PROCESSING" ||
-              data.status === "PUBLISHED") && (
+            {data.status === "CREATED" && (
               <Button
                 variant="outline"
                 size="sm"
